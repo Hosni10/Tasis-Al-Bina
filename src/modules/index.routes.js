b0155e23ea6ced0c4b04c2c9e559a3a1256e221d
mohammedhosni25@gmail.com <mohammedhosni25@gmail.com>
2025-01-26 18:56:01 +0200

    updates in index.routes.js

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -1,7 +1,8 @@
 import authRouter from "./auth/auth.routes.js"
 import blogsRouter from "./blogs/blogs.routes.js"
 import reviewsRouter from "./reviews/reviews.routes.js"
-import questionRouter from "./questions/questions.routes.js"import unitRouter from "./units/units.router.js"
+import questionRouter from "./questions/questions.routes.js"
+import unitRouter from "./units/units.router.js"
 
 
 export {
