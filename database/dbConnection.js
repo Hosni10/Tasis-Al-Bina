4cbf23dd8fc81897151fd60c5e66fb4b4827d45f
Eslam Bashry <eslamhussin600@gmail.com>
2025-01-28 23:44:22 +0200

    merg

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -7,7 +7,7 @@ config({path: path.resolve('./config/.env')})
 export const dbConnection = mongoose.connect(
   process.env.MONGO_URL,
 ).then(()=>{
-    console.log('Database connected successfully'.bgGreen); 
+    console.log('Database connected successfully  ✔️  '.bgGreen);  
 }).catch((err)=>{
     console.error('Error connecting to database:'.bgRed, err);
 });
